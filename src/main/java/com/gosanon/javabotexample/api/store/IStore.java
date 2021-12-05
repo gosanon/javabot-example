4651c34630a446eaa2a11c061bc64179347b2d19
gosanon <93736163+gosanon@users.noreply.github.com>
2021-12-05 17:50:33 +0500

    remove IStore useless dependency

--- src/main/java/com/gosanon/javabotexample/api/store/IStore.java
+++ src/main/java/com/gosanon/javabotexample/api/store/IStore.java
@@ -1,7 +1,5 @@
 package com.gosanon.javabotexample.api.store;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 public interface IStore {
     String getRecord(String id);
     void resetRecord(String id);
